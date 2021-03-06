412d01770696f1f2c11f888e7ab838a48f49f942
asreenz <72838452+asreenz@users.noreply.github.com>
2021-03-06 11:09:51 -0500

    Add files via upload

---
+++ my.js
@@ -0,0 +1,5 @@
+document.getElementById("videos1").poster = "i01_hayeswebsite-video1.png";
+document.getElementById("videos2").poster = "i01_hayeswebsite-video2.png";
+document.getElementById("videos3").poster = "i01_hayeswebsite-video3.png";
+document.getElementById("videos4").poster = "i02_hayeswebsite-video4.png";
+document.getElementById("videos5").poster = "i03_hayeswebsite-video5.png";
